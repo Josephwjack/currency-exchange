c422d185083668170e6427c15853fd6ee9cc7709
Josephwjack <joseph.w.jack@gmail.com>
2022-07-01 14:12:33 -0700

    getting api response console. WIP values to different currencies

--- src/index.js
+++ src/index.js
@@ -18,16 +18,14 @@ $(document).ready(function() {
   $('#convert').click(function() {
     let currencyInput = $('#currencyInput').val();
     makeApiCall(currencyInput);
-  })
+  });
 });
 
 
-    function getElements(response) {
-      if (response !== null) {
-      let inputDollar = $('#dollarInput').val()
-      $('#output').text(parseInt(response * inputDollar));
-      
-    
+function getElements(response) {
+  if (response !== null) {
+    let inputDollar = $('#dollarInput').val();
+    $('#output').text(parseInt(response * inputDollar)); 
     } else {
     $('.showErrors').text(`There was an error processing your request. Please try a different currency.`)
     }
