92e2a1fbb344c8360b92879f58c50a0975f58cfd
Josephwjack <joseph.w.jack@gmail.com>
2022-07-01 12:14:45 -0700

    add value to input dollar and add error option. now to add values todifferent currencies

--- src/index.js
+++ src/index.js
@@ -5,17 +5,29 @@ import './css/styles.css';
 import ExchangeService from './exchange-service.js';
 
 
-async function makeApiCall() {
-  const response = await ExchangeService.getChange();
-  getElements(response);
+
+async function makeApiCall(currencyInput) {
+  const response = await ExchangeService.getChange(currencyInput);
+  getElements(response.conversion_rates[currencyInput]);
 }
+
+
 
 $(document).ready(function() {
   $('#convert').click(function() {
-    let dollarInput = $('#dollarInput').val();
-    let currencyInput = $('#currency')
-    makeApiCall();
+    let currencyInput = $('#currencyInput').val();
+    makeApiCall(currencyInput);
   })
 });
 
 
+    function getElements(response) {
+      if (response !== null) {
+      let inputDollar = $('#dollarInput').val()
+      $('#output').text(parseInt(response * inputDollar));
+      
+    
+    } else {
+    $('.showErrors').text(`There was an error processing your request. Please try a different currency.`)
+    }
+  }
